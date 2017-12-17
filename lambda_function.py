a69b08d8407f87c19bcd67ad0285338ef68fe96d
Chad Burkins <chad.burkins@gmail.com>
2017-12-17 19:31:19 +0000

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,57 @@
+import json
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    
+
+    if (('queryStringParameters' in event.keys()) and (event['queryStringParameters'] != None)):
+        
+
+        logger.info(event)
+
+        params = event["queryStringParameters"]
+        
+        logger.info(params);
+        logger.info(type(params))
+        
+        if "statement" in params:
+    
+            userStatement = params["statement"];
+            comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+            # compResult = comprehend.detect_sentiment(Text = "This makes me very unhappy", LanguageCode='en')
+            compResult = comprehend.detect_sentiment(Text = userStatement, LanguageCode='en')
+    
+            # dataPkg = { 'username': 'El Roberto', 'id': 42 };
+            dataPkg = { 'results': compResult };
+            dataPkg = { 'userStatement': userStatement, 'results': compResult };
+            # Original CORS: "http://ec2-54-210-100-224.compute-1.amazonaws.com"
+            return {
+                'statusCode': 200,
+                'headers': { 'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "http://awsafjnva1004.jnj.com",
+                    "Access-Control-Allow-Credentials": "true"
+                 },
+                'body': json.dumps(dataPkg)
+            }
+        else:
+            return {
+                'statusCode': 422,
+                'headers': { 'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "http://amazonaws.com",
+                    "Access-Control-Allow-Credentials": "true"
+                },
+                'body': json.dumps({ 'error': 'missing parameters'})
+            }
+    else:
+        return {
+                'statusCode': 422,
+                'headers': { 'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "http://amazonaws.com",
+                    "Access-Control-Allow-Credentials": "true"
+                },
+                'body': json.dumps({ 'error': 'Zero parameters sent'})
+            }
